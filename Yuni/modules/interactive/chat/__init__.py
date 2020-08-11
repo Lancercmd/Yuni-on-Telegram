7ad8c7b7e6accf005e2b5fd1e6b7af90029f4719
Lancercmd <59906398+Lancercmd@users.noreply.github.com>
2020-08-11 17:20:32 +0800

    Code cleaning

--- Yuni/modules/interactive/chat/__init__.py
+++ Yuni/modules/interactive/chat/__init__.py
@@ -1,12 +1,3 @@
-import config
-
-from aiogram import Bot, Dispatcher
-
-API_TOKEN = config.token
-
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-
 async def start(message):
     await message.answer('''Yuni on Telegram
 Powered by aiogram.''')
